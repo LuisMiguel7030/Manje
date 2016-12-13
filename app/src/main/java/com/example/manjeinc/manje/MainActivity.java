af9f1afca2586a274df54a5b66aa15f50464b9ad
Luis Miguel <lucho7030@gmail.com>
2016-12-13 15:42:53 -0500

    add bottons

---
+++ app/src/main/java/com/example/manjeinc/manje/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.manjeinc.manje;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+
+public class MainActivity extends Activity {
+
+    TextView manje, email, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        manje = (TextView)findViewById(R.id.manje);
+        email = (TextView)findViewById(R.id.textEmail);
+        password = (TextView)findViewById(R.id.textPassword);
+
+        String font_path = "font/Roboto-Black.ttf";
+
+        Typeface TF = Typeface.createFromAsset(getAssets(),font_path);
+        manje.setTypeface(TF);
+        email.setTypeface(TF);
+        password.setTypeface(TF);
+
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/manjeinc/manje/SplashScreenActivity.java
@@ -0,0 +1,49 @@
+package com.example.manjeinc.manje;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * Created by MANJE INC on 12/8/2016.
+ */
+
+public class SplashScreenActivity extends Activity{
+
+    //configurar el timpo del splash
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void  onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // comenzar la nueva actividad
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                // Cerramos la actividad para que el usuario no pueda volver atras
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+
+
+    }
+
+
+}
